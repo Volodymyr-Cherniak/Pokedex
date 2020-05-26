81adf82cc5c710736b08e08517c9768c687eb7c8
Volodymyr <vovastalker26@gmail.com>
2020-05-26 12:45:50 +0300

    upd url

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,7 +3,7 @@ export const GET_NEXT_POKEMONS = 'GET_NEXT_POKEMONS';
 export const OPEN_INFO = 'OPEN_INFO';
 export const initialState = {
   pokemon: [],
-  url: 'http://pokeapi.co/api/v2/pokemon/',
+  url: 'https://cors-anywhere.herokuapp.com/http://pokeapi.co/api/v2/pokemon/',
   limit: '?limit=12',
   nextPage: '',
   isOpen: false,
