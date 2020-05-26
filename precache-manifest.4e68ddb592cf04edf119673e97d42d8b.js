b37f70bb52740d7384db24d45f414c6847e7ef8b
Volodymyr <vovastalker26@gmail.com>
2020-05-26 11:59:06 +0300

    Updates

---
+++ precache-manifest.4e68ddb592cf04edf119673e97d42d8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82017b6bfeb617cba5a435991679764",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "02b3324fec97b2ba4a34",
+    "url": "/Pokedex/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f32400644ea1e96ce158",
+    "url": "/Pokedex/static/css/main.54fac4ea.chunk.css"
+  },
+  {
+    "revision": "02b3324fec97b2ba4a34",
+    "url": "/Pokedex/static/js/2.a1df9e9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokedex/static/js/2.a1df9e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32400644ea1e96ce158",
+    "url": "/Pokedex/static/js/main.ded6214c.chunk.js"
+  },
+  {
+    "revision": "c129e04ae51e70ff4d15",
+    "url": "/Pokedex/static/js/runtime-main.fda359d6.js"
+  }
+]);
